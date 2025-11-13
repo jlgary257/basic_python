2374fc33f48108211ab2b85967475b32d7578e14
JOEL GARY <b032110294@student.utem.edu.my>
2025-11-14 01:24:24 +0800

    Complete datatype

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from breakpoint import print_hi
 from print_datatype import print_datatype
 
-name = "Jojo Gary"
-age = 23
-weight = 100.3
-food = ["cat food", "chicken"]
-isFat = True
+name = "Jojo Gary" #str
+age = 23 #int
+weight = 100.3 #float
+food = ["cat food", "chicken"] #list
+isFat = True #boolean
 
 
 if __name__ == '__main__':
-    print_datatype(weight)
+    print_datatype(food)

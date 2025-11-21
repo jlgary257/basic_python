19dc1da645cc2309971498387b2d0dfb158fc1db
JOEL GARY <b032110294@student.utem.edu.my>
2025-11-21 13:17:35 +0800

    edit into main - Prime

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from logging import setLoggerClass
 
 from breakpoint import print_hi
 from for_loop import for_animal, for_link, num_range, range_list, range_word
+from prime_number import show_array, prime_no
 from print_datatype import print_datatype
 from selection import cat_fatness, cat_inner_select
 
@@ -12,14 +13,19 @@ food = ["cat food", "chicken"] #list
 isFat = True #boolean
 words = ['cat','dog','lion','penguin','fish', 'eagle']
 link_words = {'A':'cat','B':'dog','C':'cat','D':'penguin','E':'cat','F': 'eagle'}
+s = 1
+l = 100
+num_list = []
 
 if __name__ == '__main__':
     #print_datatype(food)
     #cat_fatness(weight)
     #cat_inner_select(age)
-    for_animal(words)
-    for_link(link_words)
-    #num_range(10)
-    #range_list(3,10)
-    range_word(words)
-    print(list(enumerate(words)))
+    # for_animal(words)
+    # for_link(link_words)
+    # #num_range(10)
+    # #range_list(3,10)
+    # range_word(words)
+    # print(list(enumerate(words)))
+    prime_no(num_list,2,30)
+   # show_array(num_list)
